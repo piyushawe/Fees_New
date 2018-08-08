920a3727a51c9c8a92973a8d9e9ae16a21c0a639
piyush <piyush.nautiyal@franciscansolutions.in>
2018-08-08 15:36:45 +0530

    Updation on sequential execution of tests

--- src/test/java/Fees_New/Step_Def.java
+++ src/test/java/Fees_New/Step_Def.java
@@ -1,22 +1,30 @@
 package Fees_New;
 
 import cucumber.api.java.en.Given;
+import gherkin.lexer.Th;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.concurrent.TimeUnit;
 
 public class Step_Def {
     @Given("^opening toolsqa$")
     public void opening_toolsqa() throws Throwable {
+        //App.returnDriver().manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);
         // Write code here that turns the phrase above into concrete actions
         System.out.println("This is Background");
         //App.returnDriver().navigate().to("http://toolsqa.com/automation-practice-form/");
         App.returnDriver().navigate().to("http://qaerp.franciscanecare.net/Secure/default.aspx");
         App.returnDriver().findElement(By.id("txtUserName")).sendKeys("admin");
         App.returnDriver().findElement(By.id("txtPassword")).sendKeys("Admin#franciscan");
+        Thread.sleep(2000);
         App.returnDriver().findElement(By.id("btnLogin")).click();
+        //App.returnDriver().findElement(By.xpath("//a[@href='/FeeManagement/Default.aspx']")).click();
         Utility.getwindow();
         if (Utility.desiredWindow) {
 
         }else{
+            Thread.sleep(4000);
             App.returnDriver().findElement(By.xpath("//a[@href='/FeeManagement/Default.aspx']")).click();
         }
     }
