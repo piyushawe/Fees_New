945ad08d67e217dcd405f9804c0872d3254ac823
piyush <piyushnautiyal153@gmail.com>
2018-08-10 21:44:50 +0530

    wdfs

--- src/main/java/Webdriver_Support/WebDriverInitialization.java
+++ src/main/java/Webdriver_Support/WebDriverInitialization.java
@@ -8,28 +8,35 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  * Hello world!
  *
  */
-public class WebDriverInitialization
-{  private static WebDriver web;
-    private WebDriverInitialization()
-    {
-    }
-   static public void intiliazedriver(String Browser)
-    {
-       if(Browser.equalsIgnoreCase("chrome"))
-       {
-           System.setProperty("webdriver.chrome.driver","D:\\selenium drivers\\chromedriver_win32\\chromedriver.exe");
-           web=new ChromeDriver();
-       }
-       if(Browser.equalsIgnoreCase("firefox"))
-       {
-           System.setProperty("webdriver.gecko.driver","D:\\selenium drivers\\geckodriver-v0.21.0-win64\\geckodriver.exe");
-           web=new FirefoxDriver();
-       }
+public class WebDriverInitialization {
+    private static WebDriver web;
 
+    private WebDriverInitialization() {
     }
-    static public WebDriver returnDriver()
-    {
-        return web;
 
+    static public void intiliazedriver(String Browser) {
+        switch (Browser.toLowerCase()) {
+            case "chrome": {
+                System.setProperty("webdriver.chrome.driver", "D:\\selenium drivers\\chromedriver_win32\\chromedriver.exe");
+                web = new ChromeDriver();
+                break;
+            }
+            case "firefox":{
+                System.setProperty("webdriver.gecko.driver", "D:\\selenium drivers\\geckodriver-v0.21.0-win64\\geckodriver.exe");
+                web = new FirefoxDriver();
+                break;
+            }
+            default:
+            {
+                System.exit(-2);
+
+            }
+        }
+
+        static public WebDriver returnDriver ()
+        {
+            return web;
+
+        }
     }
 }
